dd024fdef4d44c10890baed68a758c696e57cf59
= <=>
2023-01-07 15:22:31 -0500

    youtube logo update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -19,8 +19,10 @@
             </form>
         </div>
     </div>-->
-    <div class="legal space-y-2 mt-10">
-        <div class="flex justify-center"><a href="https://www.youtube.com/channel/UC21zc6VKI4nkTqSVGJj3XhA" class="w-20" target="blank"><img class="mr-2" src="/images/yt_logo_rgb_light.png" alt="YouTube icon" ></a></div>
+    <div class="legal space-y-2 mt-6">
+        <div class="flex justify-center">
+            <a href="https://www.youtube.com/channel/UC21zc6VKI4nkTqSVGJj3XhA" style="width: 8rem;" target="blank"><img src="/images/yt_logo_rgb_light.png" alt="YouTube icon" ></a>
+        </div>
         <div>&copy; <?php echo date("Y") ?> Uncover Your Fit</div>
         <div><a href="/privacy">Privacy Policy</a></div>
         <div><a href="/terms">Terms of Use</a></div>
