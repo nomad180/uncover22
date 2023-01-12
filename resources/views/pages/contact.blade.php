9cd9ab686bee1e58bc8095dbff4292d5e093b084
= <=>
2023-01-12 10:35:58 -0500

    created contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,6 +1,8 @@
-@section('title'){{'About | Uncover Your Fit'}}@endsection
-@section('description'){{'This page provides information about Uncover Your Fit and its approach to coaching.'}}@endsection
+@section('title'){{'Contact Us | Uncover Your Fit'}}@endsection
+@section('description'){{'This page provides contact information for Uncover Your Fit.'}}@endsection
 @section('author'){{'Damon Leach'}}@endsection
 <x-layout>
-Contact Us Page
+    <div class="w-full flex justify-center">
+        <img src="/images/construction ws.jpg" alt="">
+    </div>
 </x-layout>
