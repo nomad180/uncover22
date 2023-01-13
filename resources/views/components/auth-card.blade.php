bf2a43ca85dc30f56fb4c5aa3218fa0df1f07d2d
= <=>
2023-01-12 21:13:54 -0500

    background on login

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,4 +1,4 @@
-<div class="lg:min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div class="lg:min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
     <div class="w-full sm:max-w-md mt-6 mb-6 px-6 py-4 bg-white shadow-xl shadow-secondary/50 border border-secondary/30 overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
