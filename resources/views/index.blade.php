fd6317fd2c68fa1c5eb4987c627a91ff2039063f
= <=>
2023-10-18 18:15:39 -0400

    add shadow to index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
     </div>
     <div class="px-4 md:px-8 lg:px-16 xl:px-32">
         <p class="text-5xl text-center text-secondary mt-2 handwriting">How we can help ...</p>
-            <div class="mt-2 flex flex-col md:flex-row pages transition duration-1000 hover:border hover:border-secondary/30 hover:shadow-lg hover:shadow-secondary/50 sm:rounded-lg pb-8 mb-10 p-4">
+            <div class="mt-2 flex flex-col md:flex-row pages">
                 <div class="md:w-1/2 mx-auto md:mr-6 mt-4">
                     <img class="rounded-xl shadow-lg shadow-secondary" src="/images/journey.jpg" alt="A person holding a compass">
                 </div>
