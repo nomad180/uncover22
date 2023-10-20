1702b52bcea3507f85f5c248301e97d8e0538126
= <=>
2023-10-20 17:18:16 -0400

    added CTA

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -36,7 +36,7 @@
             @endif
         </menu>
         <div class="hidden lg:flex justify-end m-auto">
-            <a href="/coaching" class="text-sm xl:text-xl px-4 py-2 bg-secondary text-white hover:bg-primary focus:bg-primary rounded-full">
+            <a href="/coaching" class="text-sm xl:text-xl px-4 py-2 bg-secondary text-white hover:bg-primary hover:scale-105 focus:bg-primary rounded-full">
                 Explore Programs
             </a>
         </div>
