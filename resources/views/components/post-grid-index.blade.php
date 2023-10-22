48189f1fb7da503aa349545f0cb1095ef009bcc7
= <=>
2023-10-22 14:30:34 -0400

    fixed blog spacing on index

--- resources/views/components/post-grid-index.blade.php
+++ resources/views/components/post-grid-index.blade.php
@@ -2,7 +2,7 @@
 
 @if ($posts->count(1)>0)
     @foreach ($posts as $post)
-        <x-post-card :post="$post" class="{{ $loop->iteration <4 ? 'md:w-1/3' : 'hidden' }}"/>
+        <x-post-card :post="$post" class="{{ $loop->iteration <4 ? 'md:w-1/3 md:mx-4' : 'hidden' }}"/>
     @endforeach
 @endif
 
