45bf6e662d4e5346910b2df18a854dd1fec32311
= <=>
2023-10-22 15:51:28 -0400

    test

--- resources/views/components/post-grid-index.blade.php
+++ resources/views/components/post-grid-index.blade.php
@@ -2,7 +2,7 @@
 
 @if ($posts->count(1)>0)
     @foreach ($posts as $post)
-        <x-post-card :post="$post" class="{{ $loop->iteration <4 ? 'md:w-1/3 md:px-4' : 'hidden' }}"/>
+        <x-post-card :post="$post" class="{{ $loop->iteration <4 ? 'md:w-1/3' : 'hidden' }}"/>
     @endforeach
 @endif
 
