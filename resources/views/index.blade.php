43f9e5d340cd11aac36fbabf562343d94638a9fa
= <=>
2023-10-22 22:11:10 -0400

    added features section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -55,18 +55,103 @@
                     <p class="text-left">Ah, the Zen zone! Health coaching equips you with strategies to master stress. It's like having a personal meditation guru on speed dial, helping you find tranquility in life's chaos.</p>
                 </div>
             </div>
-            <div class="flex justify-center w-full text-2xl">
-                <a href="/coaching" class="hover:scale-105 inline-flex items-center py-2 px-8 pb-3 bg-secondary rounded-full text-white hover:bg-primary">Explore Coaching</a>
+        </div>
+        <h2 class="mt-16 xl:mt-24 2xl:mt-44 text-5xl 2xl:text-7xl text-center handwriting2">Features</h2>
+        <div class="flex flex-col md:flex-row flex-wrap md:mt-8">
+            <div class="flex md:w-1/2 mb-6 md:px-4 justify-center">
+                <div class="transition duration-1000 hover:border hover:border-secondary/30 hover:shadow-lg hover:shadow-secondary/50 rounded-lg pb-8 mb-10 p-4 w-full">
+                    <ul>
+                        <li class="accordion mt-6">One-on-One Coaching</li>
+                            <div class="panel">
+                                <p class="py-2">Picture this, folks. It's just you and me in the ring, no distractions, no sharing the spotlight. One-on-one coaching means you get my undivided attention. It's like having a personal trainer for your life, a Sherpa guiding you up your own fitness Everest.</p>
+                            </div>
+                        <li class="accordion">Tailored Nutrition and Wellness Plan</li>
+                            <div class="panel">
+                                <p class="py-2">Think of it as a custom-made suit for your health. Your unique needs, preferences, and goals are the fabric, and I'm the tailor. We'll stitch together a plan that fits you like a glove, not something off the rack. It's about eating and living in a way that suits your body and your life like nothing else.</p>
+                            </div>
+                        <li class="accordion">Accountability</li>
+                            <div class="panel">
+                                <p class="py-2">We all need a gentle push from time to time, don't we? That's where accountability comes in. Imagine me as your friendly GPS, keeping you on the right path. I'll hold you to your commitments, cheer you on when you're crushing it, and nudge you when you stray.</p>
+                            </div>
+                        <li class="accordion">SMART Goals</li>
+                            <div class="panel">
+                                <p class="py-2">SMART goals are the North Star of our journey. They're Specific, Measurable, Achievable, Relevant, and Time-bound. No more vague promises or endless "somedays." We set clear, actionable goals that get you where you want to go.</p>
+                            </div>
+                        <li class="accordion">Habit-Based</li>
+                            <div class="panel">
+                                <p class="py-2">Small changes, big impact. It's like compound interest for your health. We'll focus on building sustainable habits that stick. Remember, Rome wasn't built in a day, and neither is a healthier you.</p>
+                            </div>
+                    </ul>
+                </div>
+            </div>
+            <div class="flex md:w-1/2 mb-6 md:px-4 justify-center">
+                <div class="transition duration-1000 hover:border hover:border-secondary/30 hover:shadow-lg hover:shadow-secondary/50 rounded-lg pb-8 mb-10 p-4 w-full">
+                    <ul>
+                        <li class="accordion mt-6">Physical Health</li>
+                            <div class="panel">
+                                <p class="py-2">Your body is a temple, and we're the renovation team. We'll tackle nutrition, exercise, sleep, and all things physical. It's about feeling good, moving well, and staying strong for the long haul.</p>
+                            </div>
+                        <li class="accordion">Mental Health</li>
+                            <div class="panel">
+                                <p class="py-2">Your mind is the engine that drives it all. We'll dive into stress management, sleep optimization, and other mental strategies. After all, it's hard to run a marathon if your head's not in the game.</p>
+                            </div>
+                        <li class="accordion">90-Minute Intro Call</li>
+                            <div class="panel">
+                                <p class="py-2">Think of it as a first date, but with more questions. We'll get to know each other, dive into your goals, and start charting the course. It's a deep dive into your health history, preferences, and dreams.</p>
+                            </div>
+                        <li class="accordion">60-Minute Biweekly Check-Ins</li>
+                            <div class="panel">
+                                <p class="py-2">These are like pit stops during a race. We'll track your progress, address any hurdles, and make adjustments. It's our chance to fine-tune the engine and keep you on course.</p>
+                            </div>
+                        <li class="accordion">Unlimited Email Questions</li>
+                            <div class="panel">
+                                <p class="py-2">Ever have a burning question at 2 a.m.? Shoot me an email, and I'll be your 24/7 health hotline. It's like having your personal Google for health.</p>
+                            </div>
+                    </ul>
+                </div>
             </div>
         </div>
         <!--Add testimonial section here-->
-        <!--Add features here-up to 10 in a bulleted list-->
+        <!--Add features here-up to 10 in a bulleted list
+         1. One-on-One Coaching (Not Group)
+         2. Tailored Nutrition and Wellness Plan
+         3. Accountability
+         4. SMART Goals
+         5. Habit based
+         6. Phyiscal Health
+         7. Mental Health
+         8. 90-minute intro call
+         9. 60-minute biweekly checkins
+         10. Unlimited email questions
+         11. Easy to follow lifestyle changes
+        -->
         <!--Add FAQs in dropdown here- Proactively handle objections in this section: Top 5 or 6 questions-->
         <!--Super direct call to action-->
+        <div class="flex justify-center w-full text-2xl">
+            <a href="/coaching" class="hover:scale-105 inline-flex items-center py-2 px-8 pb-3 bg-secondary rounded-full text-white hover:bg-primary">Explore Coaching</a>
+        </div>
     </div>
     <!-- Calendly badge widget begin -->
     <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
     <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
     <script type="text/javascript">window.onload = function() { Calendly.initBadgeWidget({ url: 'https://calendly.com/uncover_your_fit', text: 'Schedule Coaching Discovery Call', color: '#354b75', textColor: '#ffffff', branding: true }); }</script>
     <!-- Calendly badge widget end -->
+    <!--Accordion begin-->
+    <script>
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+    </script>
+    <!--Accordion end-->
 </x-layout>
