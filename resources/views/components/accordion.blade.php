51f64ba51eb0cd269046ad49e3329c2acfcb541a
= <=>
2023-10-25 23:29:18 -0400

    mailchimp

--- resources/views/components/accordion.blade.php
+++ resources/views/components/accordion.blade.php
@@ -20,3 +20,4 @@ for (let i = 0; i < acc.length; i++) {
 }
 </script>
 <!--Accordion end-->
+<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/bfa28f3d091acf6cc4a310cf2/138c5e8294bf0591d2fe2d284.js");</script>
