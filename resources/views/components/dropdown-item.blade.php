56806c16651a5ba5a4a724084ae9e5562613b580
= <=>
2023-10-31 10:30:23 -0400

    font color change

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,7 +1,7 @@
 @props(['active' => false])
 @php
     $classes = 'inline-flex items-center text-secondary px-4 text-base hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-white focus:text-white rounded-full border-2 border-white shadow-lg shadow-white';
-    if ($active) $classes = 'inline-flex items-center px-4 text-base text-primary hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-white rounded-full border-2 border-white shadow-lg shadow-white';
+    if ($active) $classes = 'inline-flex items-center px-4 text-base text-primary font-semibold hover:font-normal hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-white rounded-full border-2 border-white shadow-lg shadow-white';
 @endphp
 <a {{ $attributes(['class' => $classes]) }}>
 {{ $slot }}</a>
