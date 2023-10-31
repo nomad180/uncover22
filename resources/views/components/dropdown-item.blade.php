7612ed2858f74f72f22b8bb408b742cf581f3c2b
= <=>
2023-10-31 10:06:03 -0400

    style

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,6 +1,6 @@
 @props(['active' => false])
 @php
-    $classes = 'inline-flex items-center text-secondary px-4 text-base hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-white focus:text-white rounded-full';
+    $classes = 'inline-flex items-center text-secondary px-4 text-base hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-white focus:text-white rounded-full border-2 border-white shadow-lg shadow-white';
     if ($active) $classes = 'inline-flex items-center px-4 text-base text-primary hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-white rounded-full';
 @endphp
 <a {{ $attributes(['class' => $classes]) }}>
--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'text-primary px-4 text-base hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-white focus:bg-secondary hover:text-white focus:text-white rounded-full'
-            : 'text-secondary px-4 text-base hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-whitefocus:bg-secondary hover:text-white focus:text-white rounded-full';
+            ? 'text-primary border-2 border-white shadow-lg shadow-white mx-1 px-4 text-base hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-white focus:bg-secondary hover:text-white focus:text-white rounded-full'
+            : 'text-secondary border-2 border-white shadow-lg shadow-white mx-1 px-4 text-base hover:bg-secondary hover:border-2 hover:border-zinc-300 hover:shadow-lg hover:shadow-zinc-400 hover:text-whitefocus:bg-secondary hover:text-white focus:text-white rounded-full';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
