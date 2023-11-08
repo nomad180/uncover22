fec8312dad657a7ee9293a7d67bc8e060b7423a6
= <=>
2023-11-07 19:08:32 -0500

    search

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,5 +1,5 @@
-<div class="rounded-xl border border-primary text-secondary px-3 py-2 mt-4 mb-2">
-    <form method="GET" action="/blog" class="border focus:border-primary">
+<div class="rounded-xl border border-primary text-secondary px-3 pt-4 mt-4 mb-2">
+    <form method="GET" action="/blog" class="border focus:border-primary ">
         @if (request('category'))
             <input type="hidden" name="category" value="{{ request('category') }}">
         @endif
