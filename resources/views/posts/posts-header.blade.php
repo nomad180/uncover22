3ed189a829c183c849c629e937b4f9933ce1902a
= <=>
2023-11-29 16:31:02 -0500

    blog

--- resources/views/posts/posts-header.blade.php
+++ resources/views/posts/posts-header.blade.php
@@ -1,5 +1,5 @@
  <div>
-   <img class="border-b border-zinc-300 mt-8 md:mt-16 lg:hidden" src="/images/blogbanner994.jpg" alt="Banner with two bowls of oatmeal on the outsides and the text Blog in the middle" width="100%">
+   <img class="border-b border-zinc-300 mt-10 md:mt-16 lg:hidden" src="/images/blogbanner994.jpg" alt="Banner with two bowls of oatmeal on the outsides and the text Blog in the middle" width="100%">
    <img class="border-b border-zinc-300 hidden lg:flex" src="/images/blogbanner992.jpg" alt="Banner with two bowls of oatmeal on the outsides and the text Blog in the middle" width="100%"></div>
 </div>
  <header class="w-full mt-5 overflow-auto">
