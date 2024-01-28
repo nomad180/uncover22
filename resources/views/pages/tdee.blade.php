b681f844fa605335d2ca5713a2616b00799f9789
= <=>
2024-01-28 18:17:59 -0500

    change

--- resources/views/pages/tdee.blade.php
+++ resources/views/pages/tdee.blade.php
@@ -353,19 +353,19 @@
                             </div>
                         </div>
                     </div>
-                    <div class="mb-8 mt-16">
+                    <!--<div class="mb-8 mt-16">
                         <div class="p-6 pages">
                             <h2 class="text-5xl leading-tight 2xl:text-7xl 2xl:leading-tight text-center handwriting6">Not sure what to do next?</h2>
                             <p class="mt-4 text-xl xl:text-3xl mt-4 text-center">Join our private Facebook group for midlife warriors.</p>
                             <div class="flex justify-center w-full text-2xl mt-6">
                                 <a href="https://www.facebook.com/groups/1430963114164699/" target="blank" class="inline-flex items-center py-2 px-4 pb-3 border-2 border-zinc-300 hover:bg-primary shadow-lg shadow-zinc-400 bg-secondary text-white rounded-full text-center no-underline">Join Group</a>
                             </div>
-                            <!--<p class="mt-4 text-xl xl:text-3xl mt-4 text-center">Try our <span class="font-semibold">FREE</span> online challenge.</p>
+                            <p class="mt-4 text-xl xl:text-3xl mt-4 text-center">Try our <span class="font-semibold">FREE</span> online challenge.</p>
                             <div class="flex justify-center w-full text-2xl mt-6">
                                 <a href="https://uncoveryourfit.practicebetter.io/#/653804e6810ea7f6eea2308e/bookings?c=65622ecd9b22ceb79eff0f42" target="blank" class="inline-flex items-center py-2 px-4 pb-3 border-2 border-zinc-300 hover:bg-primary shadow-lg shadow-zinc-400 bg-secondary text-white rounded-full text-center no-underline">Jumpstart Your Health and Fitness (7-Day Challenge)</a>
-                            </div>-->
+                            </div>
                         </div>
-                    </div>
+                    </div>-->
                     @endif
                 </div>
             </div>
