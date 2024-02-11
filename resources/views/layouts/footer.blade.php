4f4c50d30b8af2af720826be4c1e82879278ee3c
= <=>
2024-02-10 20:19:55 -0500

    mailchimp

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,36 @@
 <footer class="bg-neutral-50 border border-t-zinc-300 text-center pb-6 ">
-    <h5 class="text-3xl mt-10 handwriting6 px-10">Subscribe to receive the latest health and fitness tips from Uncover Your Fit.</h5>
+    <div id="mc_embed_shell">
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            #mc_embed_signup{background:#fafafa;width: 100%}
+            /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+        </style>
+        <div id="mc_embed_signup" class="">
+            <form action="https://uncoveryourfit.us18.list-manage.com/subscribe/post?u=bfa28f3d091acf6cc4a310cf2&amp;id=57aa688d87&amp;f_id=00d48de6f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                <div id="mc_embed_signup_scroll"><h5 class="text-center text-3xl mt-10 handwriting6 px-10 pb-8">Subscribe to receive the latest health and fitness tips from Uncover Your Fit.</h5>
+                <div class="flex">
+                    <div class="relative inline-block mx-auto bg-white rounded-full border border-secondary flex justify-between">
+                        <div class="mc-field-group flex py-3 px-5">
+                            <label for="mce-EMAIL" class="hidden lg:inline-block pt-4"><img src="/images/mailbox-icon.svg" alt="mailbox letter">
+                            </label>
+                            <input type="email" name="EMAIL" class="required email ml-6" id="mce-EMAIL" required="" value="" placeholder="Your email address">
+                        </div>
+                        <div class="optionalParent">
+                            <div class="clear foot">
+                                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button border-2 border-zinc-300 hover:bg-primary shadow-lg shadow-zinc-400 bg-secondary text-white rounded-full text-center mt-4 mx-4 text-xl" value="Subscribe">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+        <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+    </div>
+    <!--<h5 class="text-3xl mt-10 handwriting6 px-10">Subscribe to receive the latest health and fitness tips from Uncover Your Fit.</h5>
     <div class="mt-10">
-        <div class="relative inline-block mx-auto lg:bg-white rounded-full border border-secondary">
+        <!--<div class="relative inline-block mx-auto lg:bg-white rounded-full border border-secondary">
             <form method="POST" action="/newsletter" class="flex text-sm ">
                 @csrf
                 <div class="py-3 px-5 flex items-center mt-4">
@@ -18,7 +47,7 @@
                 </button>
             </form>
         </div>
-    </div>
+    </div>-->
     <div class="legal space-y-2 mt-6 flex flex-col justify-center items-center">
         <div class="flex justify-between w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 mb-2">
              <a href="https://www.facebook.com/profile.php?id=61556091502304" class="w-14 flex text-center px-2" target="blank"><img src="/images/facebook3.png" alt="Facebook icon" ></a>
@@ -32,6 +61,14 @@
         <div><a class="text-black no-underline" href="https://uncoveryourfit.buzzsprout.com/" target="blank">Podcast</a></div>
         <div><a class="text-black no-underline" href="/privacy">Privacy Policy</a></div>
         <div><a class="text-black no-underline" href="/terms">Terms of Use</a></div>
+    </div>
+    <div id="mce-responses" class="clear foot bg-secondary rounded-full w-1/3 fixed bottom-3 right-3">
+        <div class="response text-red" id="mce-error-response" style="display: none;"></div>
+        <div class="response text-white" id="mce-success-response" style="display: none;"></div>
+    </div>
+    <div aria-hidden="true" style="position: absolute; left: -5000px;">
+        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+        <input type="text" name="b_bfa28f3d091acf6cc4a310cf2_57aa688d87" tabindex="-1" value="">
     </div>
 </footer>
 </section>
